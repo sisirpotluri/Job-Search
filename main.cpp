c0576297b8213b26fe92146294bfff6a5049a17b
sisirpotluri <58895700+sisirpotluri@users.noreply.github.com>
2020-07-05 12:20:01 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,8 +9,6 @@ using namespace std;
 
 int main() {
     
-    //bool conduct = false;
-    
     Machine job_finder;
     
     job_finder.get_training_data();
@@ -18,35 +16,6 @@ int main() {
     job_finder.request_skills();
     
     job_finder.get_result();
-    
-    /*
-    while (conduct) {
-        
-        cout << "Training the Algorithm..." << endl;
-        
-        Machine job_finder;
-        
-        job_finder.get_training_data();
-        
-        job_finder.request_skills();
-        
-        job_finder.get_result();
-        
-        cout << "Enter either 'repeat' or 'done'." << endl;
-        
-        string go;
-        
-        cin >> go;
-        
-        if (go == "repeat" || go == "Repeat") {
-            conduct = true;
-        }
-        else {
-            conduct = false;
-        }
-        
-    }
-     */
     
     cout << "Job Search Complete." << endl << endl;
     
